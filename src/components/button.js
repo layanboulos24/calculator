c5b219ed846af802c24960aa5c3fdc66245eaacb
Layan Boulos <layanboulos@Layans-MacBook-Air.local>
2025-03-26 13:55:27 +0000

    adding functionality

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Button = (props) => {
     return (
-        <input type="button" value={props.label} />
+        <button type="button" value={props.label} onClick={props.handleClick}>
+            {props.label}
+        </button>
     );
 };
 export default Button;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -4,39 +4,102 @@ import OutputScreen from "./outputScreen.js";
 import CalculatorTitle from "./calculatorTitle.js";
 
 class Calculator extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            question: '',
+            answer: '',
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+        
+    }
+
+    handleClick(event) {
+        const value = event.target.value;
+
+        console.log("Button clicked:", value);
+
+        switch (value){
+            case "=":
+                console.log("Evaluating:", this.state.question);
+                if(this.state.question !== ""){
+                    let ans = "";
+                    try{
+                        ans = eval(this.state.question);
+                    }
+                    catch(err){
+                        console.log("math error", err.message)
+                        this.setState({answer: "Math Error"});
+                        return;
+                    }
+                    if(ans === undefined || isNaN(ans)){
+                        console.log("Answer is undefined or NaN, setting to Math Error");
+                        this.setState({answer: "Math Error"});
+                    } else {
+                        console.log("calculated answer:", ans)
+                        this.setState({answer: ans, question: ""});
+                    }
+                }
+                break;
+                
+            case "Clear": {
+                this.setState({question: "", answer: ""})
+                break;
+            }
+            case "Delete": {
+                this.setState((prevState) => ({
+                    question: prevState.question.slice(0, -1)
+                }));
+                break;
+            }
+            default: {
+                this.setState((prevState) => ({
+                    question: prevState.question + value
+                }));
+                break;
+            }
+
+            
+        }
+
+        
+    }
+
+    
     render() {
         return (
             <div className="frame">
                 <CalculatorTitle value="Calculator"/>
                 <div className="mainCalc">
-                    <OutputScreen/>
+                    <OutputScreen question={this.state.question} answer={this.state.answer}/>
                     <div className="button-row">
-                        <Button label={"Clear"} /> 
-                        <Button label={"Delete"} /> 
-                        <Button label={"."} /> 
-                        <Button label={"/"} /> 
+                        <Button label={"Clear"} value="Clear" handleClick={this.handleClick}/> 
+                        <Button label={"Delete"} value="Delete" handleClick={this.handleClick}/> 
+                        <Button label={"."} value="." handleClick={this.handleClick}/> 
+                        <Button label={"/"} value="/" handleClick={this.handleClick}/> 
                     </div>
                     <div className="button-row">
-                        <Button label={"7"} /> 
-                        <Button label={"8"} /> 
-                        <Button label={"9"} /> 
-                        <Button label={"*"} /> 
+                        <Button label={"7"} value="7" handleClick={this.handleClick}/> 
+                        <Button label={"8"} value="8" handleClick={this.handleClick}/> 
+                        <Button label={"9"} value="9" handleClick={this.handleClick}/> 
+                        <Button label={"*"} value="*" handleClick={this.handleClick}/> 
                     </div>
                     <div className="button-row">
-                        <Button label={"4"} /> 
-                        <Button label={"5"} /> 
-                        <Button label={"6"} /> 
-                        <Button label={"-"} /> 
+                        <Button label={"4"} value="4" handleClick={this.handleClick}/> 
+                        <Button label={"5"} value="5" handleClick={this.handleClick}/> 
+                        <Button label={"6"} value="6" handleClick={this.handleClick}/> 
+                        <Button label={"-"} value="-" handleClick={this.handleClick}/> 
                     </div>
                     <div className="button-row">
-                        <Button label={"1"} /> 
-                        <Button label={"2"} /> 
-                        <Button label={"3"} /> 
-                        <Button label={"+"} /> 
+                        <Button label={"1"} value="1" handleClick={this.handleClick}/> 
+                        <Button label={"2"} value="2" handleClick={this.handleClick}/> 
+                        <Button label={"3"} value="3" handleClick={this.handleClick}/> 
+                        <Button label={"+"} value="+" handleClick={this.handleClick}/> 
                     </div>
                     <div className="button-row">
-                        <Button label={"0"} /> 
-                        <Button label={"="} /> 
+                        <Button label={"0"} value="0" handleClick={this.handleClick}/> 
+                        <Button label={"="} value="=" handleClick={this.handleClick}/> 
                     </div>
                     
                 </div>
